3c0f060847837c5d63303c1f2ccd9f2b688a9f40
Simon DUBOELLE <duboelle.s@gmail.com>
2019-12-19 14:56:47 +0100

    map here

--- src/components/HereMap.vue
+++ src/components/HereMap.vue
@@ -1,60 +1,95 @@
 <template>
-  <div class="here-map">
-    <div ref="map" v-bind:style="{ width: width, height: height }"></div>
-  </div>
+    <div class="here-map">
+        <div ref="map" style="width: 75%; height: 600px"></div>
+    </div>
 </template>
 
 <script>
 /* eslint-disable */
+import axios from "axios";
 export default {
   name: "HereMap",
+    props: {
+      appId: String,
+      appCode: String,
+      theme: String,
+      lat: String,
+      lng: String,
+      width: String,
+      height: String,
+      toilettelat: String,
+      toilettelon: String
+    },
   data() {
     return {
       map: {},
       platform: {},
-      info: null, 
-      latPosition: 'future latitude',
-      lonPosition: 'future longitude'
-    }
-  },
-  props: {
-    appId: String,
-    appCode: String,
-    lat: String,
-    lng: String,
-    width: String,
-    height: String,
-    toilettelat:String,
-    toilettelon:String
-  },
-  created() {
-    this.platform = new H.service.Platform({
-      "app_id": this.appId,
-      "app_code": this.appCode
-    });
+      info: null,
+      latPosition: "future latitude",
+      lonPosition: "future longitude",
+    };
   },
   mounted() {
     
+    
+    this.platform = new H.service.Platform({
+      app_id: this.appId,
+        app_code: this.appCode,
+    });
+    var layers = this.platform.createDefaultLayers();
     this.map = new H.Map(
-    this.$refs.map,
-    this.platform.createDefaultLayers().normal.map,
-    {
-    zoom: 10,
-    center: { lng: this.lng, lat: this.lat },
-    pixelRatio: window.devicePixelRatio || 1
-  }
-  );
+      this.$refs.map,
+      layers.normal.map,
+      {
+        center: {lat: 47.207223, lng: -1.555592},
+        zoom: 10,
+      }
+    );
+    var events = new H.mapevents.MapEvents(this.map);
+    var behavior = new H.mapevents.Behavior(events);
+    var ui = H.ui.UI.createdDefault(this.map, layers);
+  
+    axios
+      .get(
+        "https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_toilettes-publiques-nantes-metropole&facet=commune&facet=pole&facet=type&facet=automatique&facet=acces_pmr"
+      )
+      .then(response => {
+        const toilettes = response.data.records;
+        console.log(toilettes);
+        const wherePiss = toilettes.map(
+          (toilette, index) => toilette.fields.location
+        );
+        console.log(wherePiss);
+        this.info = wherePiss;
+      });
+    console.log("fhjdhj");
 
+    navigator.geolocation.getCurrentPosition(position => {
+      this.latPosition = position.coords.latitude;
+      this.lonPosition = position.coords.longitude;
+    });
 
-  //add marker on map
-  const berlinMarker = new H.map.Marker({
-  lat:this.toilettelat,
-  lng:this.toilettelon
-  });
-  this.map.addObject(berlinMarker)
-
-  }
-}
+    // add marker on map
+    const berlinMarker = new H.map.Marker({
+      lat: this.toilettelat,
+      lng: this.toilettelon
+    });
+    this.map.addObject(berlinMarker);
+  },
+  watch: {
+    theme(newVal, oldVal) {
+        var tiles = this.platform.getMapTileService({ "type": "base" });
+        var layer = tiles.createTileLayer(
+            "maptile",
+            newVal,
+            256,
+            "png",
+            { "style": "default" }
+        );
+        this.map.setBaseLayer(layer);
+    }
+},
+};
 </script>
 
 <style scoped></style>
